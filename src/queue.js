fc8920efe1f1cec0705762f1da71b0482ca9017e
Winsler <r.makretski@gmail.com>
2019-03-23 03:10:42 +0300

    feat: add class queue

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,31 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() == this.maxSize) {
+			throw new Error('Queue has max size');
+		}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw new Error('Queue is empty');	
+		};
+		let top = this.heap.pop();
+		return top;
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
